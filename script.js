e2278184f8fae8bcba766b7ca7a7f82c10e5eb8e
WilliamRodriguesDS <williamrodriguesdeoliveira22@gmail.com>
2024-09-22 15:58:38 -0300

    sla

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
+function escrever(mensagem){
+    let output = document.querySelector("#output");
+    output.innerHTML = mensagem;
+}
+
 function solicitarNumeroExtenso(){
-    let solicitador = parseFloat(prompt("Digite um número de 0 a 9"));
+    let solicitador = parseFloat(document.querySelector("#solicitador").value);
+    let msg;
 
     switch(solicitador){
         case 0:
-            alert("Número extenso de 0 é zero");
+            msg = "Número por extenso de 0 é zero";
+            escrever(msg);
             break;
         case 1:
-            alert("Número extenso de 1 é um");
+            msg = "Número por extenso de 1 é um";
+            escrever(msg);
             break;
         case 2:
-            alert("Número extenso de 2 é dois");
+            msg = "Número por extenso de 2 é dois";
+            escrever(msg);
             break;
         case 3:
-            alert("Número extenso de 3 é três");
+            msg = "Número por extenso de 3 é três";
+            escrever(msg);
             break;
         case 4:
-            alert("Número extenso de 4 é quatro");
+            msg = "Número por extenso de 4 é quatro";
+            escrever(msg);
             break;
         case 5:
-            alert("Número extenso de 5 é cinco");
+            msg = "Número por extenso de 5 é cinco";
+            escrever(msg);
             break;
         case 6:
-            alert("Número extenso de 6 é seis");
+            msg = "Número por extenso de 6 é seis";
+            escrever(msg);
             break;
         case 7:
-            alert("Número extenso de 7 é sete");
+            msg = "Número por extenso de 7 é sete";
+            escrever(msg);
             break;
         case 8:
-            alert("Número extenso de 8 é oito");
+            msg = "Número por extenso de 8 é oito";
+            escrever(msg);
             break;
         case 9:
-            alert("Número extenso de 9 é nove");
+            msg = "Número por extenso de 9 é nove";
+            escrever(msg);
             break;
         default:
-            alert("Número fora do intervalo, tente novamente!");
-            return solicitarNumeroExtenso();
+            msg = "Número fora do intervalo, tente novamente!";
+            escrever(msg);
     }
 }
